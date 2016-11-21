3524e7e710b7329aaeb909a1ba8b6ba376c3bd08
chrisguttandin <chrisguttandin@media-codings.com>
2016-11-21 22:47:17 +0100

    change name of minified file

--- config/grunt/uglify.js
+++ config/grunt/uglify.js
@@ -1,7 +1,7 @@
 module.exports = {
     bundle: {
         files: {
-            'build/es5/bundle.min.js': [
+            'build/es5/script.min.js': [
                 'build/es5/bundle.js'
             ]
         }
