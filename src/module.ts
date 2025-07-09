b33c27587307726856a917803725049fabb56ebf
Christoph Guttandin <chrisguttandin@media-codings.com>
2025-07-09 22:43:02 +0200

    refactor: extract factory functions

--- src/module.ts
+++ src/module.ts
@@ -1,6 +1,10 @@
 import { createBroker } from 'broker-factory';
 import { generateUniqueNumber } from 'fast-unique-numbers';
 import { TWorkerTimersWorkerDefinition } from 'worker-timers-worker';
+import { createClearIntervalFactory } from './factories/clear-interval-factory';
+import { createClearTimeoutFactory } from './factories/clear-timeout-factory';
+import { createSetIntervalFactory } from './factories/set-interval-factory';
+import { createSetTimeoutFactory } from './factories/set-timeout-factory';
 import { IWorkerTimersBrokerDefinition } from './interfaces';
 import { TWorkerTimersBrokerLoader, TWorkerTimersBrokerWrapper } from './types';
 
@@ -15,94 +19,22 @@ export * from './types/index';
 const scheduledIntervalsState: Map<number, null | symbol> = new Map([[0, null]]); // tslint:disable-line no-empty
 const scheduledTimeoutsState: Map<number, null | symbol> = new Map([[0, null]]); // tslint:disable-line no-empty
 
+const createClearInterval = createClearIntervalFactory(scheduledIntervalsState);
+const createClearTimeout = createClearTimeoutFactory(scheduledTimeoutsState);
+const createSetInterval = createSetIntervalFactory(generateUniqueNumber, scheduledIntervalsState);
+const createSetTimeout = createSetTimeoutFactory(generateUniqueNumber, scheduledTimeoutsState);
+
 export const wrap: TWorkerTimersBrokerWrapper = createBroker<IWorkerTimersBrokerDefinition, TWorkerTimersWorkerDefinition>({
-    clearInterval: ({ call }) => {
-        return (timerId) => {
-            if (typeof scheduledIntervalsState.get(timerId) === 'symbol') {
-                scheduledIntervalsState.set(timerId, null);
-
-                call('clear', { timerId, timerType: 'interval' }).then(() => {
-                    scheduledIntervalsState.delete(timerId);
-                });
-            }
-        };
-    },
-    clearTimeout: ({ call }) => {
-        return (timerId) => {
-            if (typeof scheduledTimeoutsState.get(timerId) === 'symbol') {
-                scheduledTimeoutsState.set(timerId, null);
-
-                call('clear', { timerId, timerType: 'timeout' }).then(() => {
-                    scheduledTimeoutsState.delete(timerId);
-                });
-            }
-        };
-    },
-    setInterval: ({ call }) => {
-        return (func: Function, delay = 0, ...args: any[]) => {
-            const symbol = Symbol();
-            const timerId = generateUniqueNumber(scheduledIntervalsState);
-
-            scheduledIntervalsState.set(timerId, symbol);
-
-            const schedule = () =>
-                call('set', {
-                    delay,
-                    now: performance.timeOrigin + performance.now(),
-                    timerId,
-                    timerType: 'interval'
-                }).then(() => {
-                    const state = scheduledIntervalsState.get(timerId);
-
-                    if (state === undefined) {
-                        throw new Error('The timer is in an undefined state.');
-                    }
-
-                    if (state === symbol) {
-                        func(...args);
-
-                        // Doublecheck if the interval should still be rescheduled because it could have been cleared inside of func().
-                        if (scheduledIntervalsState.get(timerId) === symbol) {
-                            schedule();
-                        }
-                    }
-                });
-
-            schedule();
-
-            return timerId;
-        };
-    },
-    setTimeout: ({ call }) => {
-        return (func: Function, delay = 0, ...args: any[]) => {
-            const symbol = Symbol();
-            const timerId = generateUniqueNumber(scheduledTimeoutsState);
-
-            scheduledTimeoutsState.set(timerId, symbol);
-
-            call('set', {
-                delay,
-                now: performance.timeOrigin + performance.now(),
-                timerId,
-                timerType: 'timeout'
-            }).then(() => {
-                const state = scheduledTimeoutsState.get(timerId);
-
-                if (state === undefined) {
-                    throw new Error('The timer is in an undefined state.');
-                }
-
-                if (state === symbol) {
-                    // A timeout can be savely deleted because it is only called once.
-                    scheduledTimeoutsState.delete(timerId);
-
-                    func(...args);
-                }
-            });
-
-            return timerId;
-        };
-    }
+    clearInterval: ({ call }) => createClearInterval((timerId) => call('clear', { timerId, timerType: 'interval' })),
+    clearTimeout: ({ call }) => createClearTimeout((timerId) => call('clear', { timerId, timerType: 'timeout' })),
+    setInterval: ({ call }) =>
+        createSetInterval((delay, timerId) =>
+            call('set', { delay, now: performance.timeOrigin + performance.now(), timerId, timerType: 'interval' })
+        ),
+    setTimeout: ({ call }) =>
+        createSetTimeout((delay, timerId) =>
+            call('set', { delay, now: performance.timeOrigin + performance.now(), timerId, timerType: 'timeout' })
+        )
 });
 
 export const load: TWorkerTimersBrokerLoader = (url: string) => {
